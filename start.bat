b99bc79c2252c0f852fe9ed901c09d2a3fbafd73
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-03-30 20:28:21 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Masha Robot
-:: Enables virtual env mode and then starts Masha
-env\scripts\activate.bat && py -m MashaRoBot
+TITLE Alita Robot
+:: Enables virtual env mode and then starts Alita
+env\scripts\activate.bat && py -m AlitaRobot
