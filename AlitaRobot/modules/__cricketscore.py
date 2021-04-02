83ad1c1a462f9058b41bee802fbc549b1f0d86c1
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-02 21:07:32 +0530

    Update __cricketscore.py

--- AlitaRobot/modules/__cricketscore.py
+++ AlitaRobot/modules/__cricketscore.py
@@ -1,12 +1,9 @@
-# credits  @chsaiujwal @daisyxrobot
-# ported to masha @Mr_Dark_Prince
-
 import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from MashaRoBot import telethn as tbot
-from telethon.tl import functions, types
+from AlitaRobot import telethn as tbot
+from AlitaRobot.tl import functions, types
 from telethon.tl.types import *
 
 
