b1364a2a5c21e5266192a8b6015b35eef741fa46
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-02 21:14:42 +0530

    Update __json.py

--- AlitaRobot/modules/__json.py
+++ AlitaRobot/modules/__json.py
@@ -1,7 +1,7 @@
 import io
-from MashaRoBot.events import register
-from MashaRoBot import telethn as borg
-from MashaRoBot import telethn as tbot
+from AlitaRobot.events import register
+from AlitaRobot import telethn as borg
+from AlitaRobot import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions, types
