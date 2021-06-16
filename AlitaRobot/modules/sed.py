01cfe305971525a84d055e825a34d39aadbff263
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-06-16 21:57:56 +0530

    Update sed.py

--- AlitaRobot/modules/sed.py
+++ AlitaRobot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from MashaRoBot import LOGGER, dispatcher
-from MashaRoBot.modules.disable import DisableAbleMessageHandler
-from MashaRoBot.modules.helper_funcs.regex_helper import infinite_loop_check
+from AlitaRobot import LOGGER, dispatcher
+from AlitaRobot.modules.disable import DisableAbleMessageHandler
+from AlitaRobot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
